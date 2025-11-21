a6b917be3f2edfbb1678958a55397e32b36a74f2
ricardoraphaeltech <riraphaelusa@gmail.com>
2025-11-21 20:29:09 -0300

    configuracao do base do vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/iphone-17-pro-website",
+  base: "/iphone-17-pro-website/",
 });
